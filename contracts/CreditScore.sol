6d3fcaa266e7f7fad4e97020f59ae9da70f04e15
jedinrk <jedi.nrk@gmail.com>
2024-07-25 15:39:09 +0400

    splitting the credit score element update functions

--- contracts/CreditScore.sol
+++ contracts/CreditScore.sol
@@ -24,12 +24,9 @@ contract CreditScore is Ownable {
     mapping(address => CreditInfo) public creditInfo;
 
     // Configuration for each attribute
-    AttributeConfig public transactionVolumeConfig =
-        AttributeConfig(100, 10000);
-    AttributeConfig public walletBalanceConfig =
-        AttributeConfig(1 ether, 100 ether);
-    AttributeConfig public transactionFrequencyConfig =
-        AttributeConfig(10, 1000);
+    AttributeConfig public transactionVolumeConfig = AttributeConfig(100, 10000);
+    AttributeConfig public walletBalanceConfig = AttributeConfig(1 ether, 100 ether);
+    AttributeConfig public transactionFrequencyConfig = AttributeConfig(10, 1000);
     AttributeConfig public transactionMixConfig = AttributeConfig(1, 10);
     AttributeConfig public newTransactionsConfig = AttributeConfig(1, 10);
 
@@ -37,10 +34,7 @@ contract CreditScore is Ownable {
     event UpdatedCreditScore(address indexed user, uint256 creditScore);
 
     modifier validMinMax(uint256 minValue, uint256 maxValue) {
-        require(
-            minValue <= maxValue,
-            "Invalid input: minValue must be less than or equal to maxValue"
-        );
+        require(minValue <= maxValue, "Invalid input: minValue must be less than or equal to maxValue");
         _;
     }
 
@@ -50,19 +44,14 @@ contract CreditScore is Ownable {
         BLOCK_INTERVAL = newInterval;
     }
 
-    function calculateAttributeScore(
-        uint256 value,
-        AttributeConfig memory config
-    ) internal pure returns (uint256) {
+    function calculateAttributeScore(uint256 value, AttributeConfig memory config) internal pure returns (uint256) {
         if (value <= config.minValue) {
             return 0; // Minimum score if value is below or equal to minValue
         } else if (value >= config.maxValue) {
             return 1000; // Maximum score if value is above or equal to maxValue
         } else {
             // Normalize value to a score between 0 and 1000
-            return
-                ((value - config.minValue) * 1000) /
-                (config.maxValue - config.minValue);
+            return ((value - config.minValue) * 1000) / (config.maxValue - config.minValue);
         }
     }
 
@@ -79,58 +68,52 @@ contract CreditScore is Ownable {
         }
     }
 
-    function updateCreditScore(
-        address user,
-        uint256 transactionVolume,
-        uint256 walletBalance,
-        uint256 transactionFrequency,
-        uint256 transactionMix,
-        uint256 newTransactions
-    ) external onlyOwner {
+    function updateTransactionVolume(address user, uint256 transactionVolume) external onlyOwner {
+        CreditInfo storage info = creditInfo[user];
+        info.transactionVolume = calculateAttributeScore(transactionVolume, transactionVolumeConfig);
+        updateCreditScore(user);
+    }
+
+    function updateWalletBalance(address user, uint256 walletBalance) external onlyOwner {
+        CreditInfo storage info = creditInfo[user];
+        info.walletBalance = calculateAttributeScore(walletBalance, walletBalanceConfig);
+        updateCreditScore(user);
+    }
+
+    function updateTransactionFrequency(address user, uint256 transactionFrequency) external onlyOwner {
+        CreditInfo storage info = creditInfo[user];
+        info.transactionFrequency = calculateAttributeScore(transactionFrequency, transactionFrequencyConfig);
+        updateCreditScore(user);
+    }
+
+    function updateTransactionMix(address user, uint256 transactionMix) external onlyOwner {
+        CreditInfo storage info = creditInfo[user];
+        info.transactionMix = calculateAttributeScore(transactionMix, transactionMixConfig);
+        updateCreditScore(user);
+    }
+
+    function updateNewTransactions(address user, uint256 newTransactions) external onlyOwner {
+        CreditInfo storage info = creditInfo[user];
+        info.newTransactions = calculateAttributeScore(newTransactions, newTransactionsConfig);
+        updateCreditScore(user);
+    }
+
+    function updateCreditScore(address user) public onlyOwner {
         CreditInfo storage info = creditInfo[user];
 
-        uint256 transactionVolumeScore = calculateAttributeScore(
-            transactionVolume,
-            transactionVolumeConfig
-        );
-        uint256 walletBalanceScore = calculateAttributeScore(
-            walletBalance,
-            walletBalanceConfig
-        );
-        uint256 transactionFrequencyScore = calculateAttributeScore(
-            transactionFrequency,
-            transactionFrequencyConfig
-        );
-        uint256 transactionMixScore = calculateAttributeScore(
-            transactionMix,
-            transactionMixConfig
-        );
-        uint256 newTransactionsScore = calculateAttributeScore(
-            newTransactions,
-            newTransactionsConfig
-        );
-
-        info.transactionVolume = transactionVolumeScore;
-        info.walletBalance = walletBalanceScore;
-        info.transactionFrequency = transactionFrequencyScore;
-        info.transactionMix = transactionMixScore;
-        info.newTransactions = newTransactionsScore;
-        info.lastUpdated = block.number;
-
         // Calculate the weighted sum
-        uint256 weightedSum = ((transactionVolumeScore * 35) +
-            (walletBalanceScore * 30) +
-            (transactionFrequencyScore * 15) +
-            (transactionMixScore * 10) +
-            (newTransactionsScore * 10)) / 100;
+        uint256 weightedSum = ((info.transactionVolume * 35) +
+                               (info.walletBalance * 30) +
+                               (info.transactionFrequency * 15) +
+                               (info.transactionMix * 10) +
+                               (info.newTransactions * 10)) / 100;
 
         // Normalize to range [300, 850]
         uint256 minScore = 300;
         uint256 maxScore = 850;
 
-        info.creditScore =
-            ((weightedSum * (maxScore - minScore)) / 1000) +
-            minScore;
+        info.creditScore = ((weightedSum * (maxScore - minScore)) / 1000) + minScore;
+        info.lastUpdated = block.number;
 
         emit UpdatedCreditScore(user, info.creditScore);
     }
@@ -144,38 +127,44 @@ contract CreditScore is Ownable {
     }
 
     // Functions to update the attribute configurations
-    function updateTransactionVolumeConfig(
-        uint256 minValue,
-        uint256 maxValue
-    ) external onlyOwner validMinMax(minValue, maxValue) {
+    function updateTransactionVolumeConfig(uint256 minValue, uint256 maxValue)
+        external
+        onlyOwner
+        validMinMax(minValue, maxValue)
+    {
+        require(minValue <= maxValue, "Invalid input: minValue must be less than or equal to maxValue");
         transactionVolumeConfig = AttributeConfig(minValue, maxValue);
     }
 
-    function updateWalletBalanceConfig(
-        uint256 minValue,
-        uint256 maxValue
-    ) external onlyOwner validMinMax(minValue, maxValue) {
+    function updateWalletBalanceConfig(uint256 minValue, uint256 maxValue)
+        external
+        onlyOwner
+        validMinMax(minValue, maxValue)
+    {
         walletBalanceConfig = AttributeConfig(minValue, maxValue);
     }
 
-    function updateTransactionFrequencyConfig(
-        uint256 minValue,
-        uint256 maxValue
-    ) external onlyOwner validMinMax(minValue, maxValue) {
+    function updateTransactionFrequencyConfig(uint256 minValue, uint256 maxValue)
+        external
+        onlyOwner
+        validMinMax(minValue, maxValue)
+    {
         transactionFrequencyConfig = AttributeConfig(minValue, maxValue);
     }
 
-    function updateTransactionMixConfig(
-        uint256 minValue,
-        uint256 maxValue
-    ) external onlyOwner validMinMax(minValue, maxValue) {
+    function updateTransactionMixConfig(uint256 minValue, uint256 maxValue)
+        external
+        onlyOwner
+        validMinMax(minValue, maxValue)
+    {
         transactionMixConfig = AttributeConfig(minValue, maxValue);
     }
 
-    function updateNewTransactionsConfig(
-        uint256 minValue,
-        uint256 maxValue
-    ) external onlyOwner validMinMax(minValue, maxValue) {
+    function updateNewTransactionsConfig(uint256 minValue, uint256 maxValue)
+        external
+        onlyOwner
+        validMinMax(minValue, maxValue)
+    {
         newTransactionsConfig = AttributeConfig(minValue, maxValue);
     }
 }
