2989ccdcf945e65419e68c4a2a096a1a41e35b60
jedinrk <jedi.nrk@gmail.com>
2024-07-27 14:01:13 +0400

    removing server.js

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const blockchainService = require('./services/blockchainService');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  blockchainService.listenToEvents(); // Start listening to blockchain events
-});
