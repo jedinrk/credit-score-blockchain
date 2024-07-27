da4154cd439169b1a144eac5d44ff8b9a982106f
jedinrk <jedi.nrk@gmail.com>
2024-07-27 13:56:08 +0400

    Adding transactions fetch service

--- backend/services/transactionService.js
+++ backend/services/transactionService.js
@@ -1,8 +1,16 @@
-const Web3 = require('web3');
+const { Web3, HttpProvider } = require('web3');
+
 const { INFURA_URL } = process.env;
 
-const web3 = new Web3(new Web3.providers.HttpProvider(INFURA_URL));
+const web3 = new Web3(new HttpProvider(INFURA_URL));
 
+/**
+ * Fetch the transactions of a given address from a last six months(approx or assumption based) 
+ * Filter transactions based on Eth transfer, DEX and DEFI protocol interactions.
+ * 
+ * TODO Add Defi protocol method references. 
+ * 
+ */
 exports.fetchTransactions = async (address) => {
   const latestBlock = await web3.eth.getBlockNumber();
   const blocksInSixMonths = Math.floor(180 * 24 * 60 * 60 / 13);
@@ -23,6 +31,7 @@ exports.fetchTransactions = async (address) => {
 
   // Filter transactions (e.g., ETH transfers, DEX transactions, etc.)
   const ethTransfers = transactionDetails.filter(tx => tx.value > 0);
+  //TODO
   const dexTransactions = transactionDetails.filter(tx =>{ /* logic to identify DEX transactions */});
   const defiTransactions = transactionDetails.filter(tx => {/* logic to identify DeFi transactions */});
 
