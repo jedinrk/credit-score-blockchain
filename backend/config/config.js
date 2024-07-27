4d7c2d892e1323fb537929baeb5c0e75344d5453
jedinrk <jedi.nrk@gmail.com>
2024-07-27 15:13:05 +0400

    added user model

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   infuraUrl: process.env.INFURA_URL,
   contractAddress: process.env.CONTRACT_ADDRESS,
-  contractABI: require('./CreditScoreABI.json')
+  contractABI: require('../abis/CreditScore.json')
 };
---
+++ backend/models/users.js
@@ -0,0 +1,4 @@
+//TODO Test User Data. To be stored in a DB or secure place.
+module.exports.users = [
+  { id: 1, username: 'admin', password: '$2a$10$SZnn/jtLI8LlZW40NSY4rOOo9Y03GLtlcAnVwgkMpqKVjC4H2AG8m' } // Using bcrypt to hash the password
+];
