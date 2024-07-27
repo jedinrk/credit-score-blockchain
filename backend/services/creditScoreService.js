ce4430fb0abad270f839f1e59fe9ab61fbac33ec
jedinrk <jedi.nrk@gmail.com>
2024-07-27 14:01:46 +0400

    adding contract method event listener logic to creditscore service

--- backend/services/creditScoreService.js
+++ backend/services/creditScoreService.js
@@ -5,4 +5,14 @@ const provider = new ethers.providers.JsonRpcProvider(config.infuraUrl);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 const contract = new ethers.Contract(config.contractAddress, config.contractABI, wallet);
 
+//Event listener service for smart contract
+/*exports.listenToEvents = () => {
+  creditScoreContract.events.CheckCreditScore()
+    .on('data', async (event) => {
+      const userAddress = event.returnValues.user;
+      console.log(`Received CheckCreditScore event for address: ${userAddress}`);
+      await transactionService.fetchTransactions(userAddress);
+    })
+};*/
+
 module.exports = contract;
